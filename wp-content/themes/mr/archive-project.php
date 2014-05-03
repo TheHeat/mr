0d30ebe83ced82b5b6555ce7956c40e5bbf240dc
Marc Heatley <marc@marcheatleydesign.com>
2014-05-03 12:59:50 +0100

    ditch archive header

--- wp-content/themes/mr/archive-project.php
+++ wp-content/themes/mr/archive-project.php
@@ -11,31 +11,13 @@ get_header(); ?>
 
 <div class="feed">
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
-
-			
-
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'content', get_post_type() ); ?>
-				<?php endwhile; ?>
-			<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-				
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_type() ); ?>
+		<?php endwhile; ?>
+	<?php else : ?>
+		<?php get_template_part( 'content', 'none' ); ?>
+	<?php endif; ?>
 </div>
-
-			
 
 <?php get_footer(); ?>
